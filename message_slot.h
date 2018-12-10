37c48893b44fbcc51abdb84fc3f22a35a6ca31d1
Eran <eranchriqui@gmail.com>
2018-12-10 20:44:59 +0200

    fixes

--- message_slot.h
+++ message_slot.h
@@ -4,18 +4,15 @@
 
 #ifndef HW3_MESSAGE_SLOT_H
 #define HW3_MESSAGE_SLOT_H
-#include <linux/ioctl.h>
 
-#define MAJOR_NUM 555
-#define MSG_SLOT_CHANNEL _IORW(MAJOR_NUM, 0, unsigned long)
+#define MAJOR_NUM 111
+#define MSG_SLOT_CHANNEL _IOWR(MAJOR_NUM, 0, unsigned long)
 
 
 #define DEVICE_RANGE_NAME "char_dev"
 #define BUF_SIZE 128
-#define CHANNELS 4
 #define DEVICE_FILE_NAME "message_slot_dev"
 #define SUCCESS 0
-#define NULL 0
 #define FALSE 0
 #define TRUE 1
 
